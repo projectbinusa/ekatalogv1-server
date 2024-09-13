949fbad6d681970e568dca35f3dc383c96218dd2
niscitacan12 <citacandrarini@gmail.com>
2024-09-13 11:46:38 +0700

    perbaikan update

--- src/main/java/com/example/ekatalogv1Server/controller/ListProdukController.java
+++ src/main/java/com/example/ekatalogv1Server/controller/ListProdukController.java
@@ -16,11 +16,13 @@ public class ListProdukController {
     @Autowired
     private ListProdukService listProdukService;
 
+    // GetAll sesuai kategori kualitas standar
     @GetMapping("/kualitas_standar/{namaKategori}")
     public List<ProdukKualitasStandar> getBarangByKategori(@PathVariable String namaKategori) {
         return listProdukService.getBarangByKategoriNama(namaKategori);
     }
 
+    // GetAll sesuai kategori kualitas tinggi
     @GetMapping("/kualitas_tinggi/{namaKategori}")
     public List<ProdukKualitasTinggi> getProdukByKategori(@PathVariable String namaKategori) {
         return listProdukService.getProdukByKategoriNama(namaKategori);
